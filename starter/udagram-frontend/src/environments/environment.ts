0bf0928a9ca4c5cc9c74c97e843181730507b19e
Mohamed El-khamisi <mohammd.alkhamisi@gmail.com>
2022-04-23 06:01:45 +0200

    circle ci

--- starter/udagram-frontend/src/environments/environment.ts
+++ starter/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://hostingapp2-env.eba-zra2p64h.us-east-1.elasticbeanstalk.com//api/v0'
+  apiHost: 'http://hostingapp2-env.eba-zra2p64h.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
