fff4bcd44f97db309e261f58517a8c276fcf1aae
Mohamed El-khamisi <mohammd.alkhamisi@gmail.com>
2022-04-26 16:37:59 +0200

    deploy CircleCi

--- starter/udagram-frontend/src/environments/environment.prod.ts
+++ starter/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://hostingapp2-env.eba-zra2p64h.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://hostingfullstack.eba-my9zsidh.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
